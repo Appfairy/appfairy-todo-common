eaa08a1a1077108e44428c4a9ebe463db5326fda
DAB0mB <emanor6@gmail.com>
2017-12-15 06:37:59 +0200

    Move view classes to common, including animations

--- todo/item/index.js
+++ todo/item/index.js
@@ -1,2 +1,17 @@
-export const css = require('./style.scss');
-export const html = require('./view.html');
+import Appfairy from 'appfairy';
+import css from './style.scss';
+import html from './view.html';
+
+class TodoItemView extends Appfairy.View(HTMLElement) {
+  initializeStyle(style) {
+    style.innerHTML = css;
+  }
+
+  initializeView(view) {
+    view.innerHTML = html;
+  }
+}
+
+Appfairy.View.define('todo-item', TodoItemView);
+
+export default TodoItemView;
--- todo/list/index.js
+++ todo/list/index.js
@@ -1,2 +1,55 @@
-export const css = require('./style.scss');
-export const html = require('./view.html');
+import Appfairy from 'appfairy';
+import css from './style.scss';
+import html from './view.html';
+
+class TodoListView extends Appfairy.View(HTMLElement) {
+  initializeStyle(style) {
+    style.innerHTML = css;
+  }
+
+  initializeView(view) {
+    view.innerHTML = html;
+
+    const todo = view.querySelector('.todo');
+
+    todo.show = function (proxy) {
+      Appfairy.Socket.prototype.show.call(this, proxy);
+
+      this.animate([
+        {
+          opacity: '0',
+          transform: 'translateX(200px)',
+        },
+        {
+          opacity: '1',
+          transform: 'translateX(0)',
+        }
+      ], {
+        duration: 500,
+        easing: 'ease',
+      });
+    };
+
+    todo.hide = function (proxy) {
+      const animation = this.animate([
+        {
+          opacity: '1',
+          transform: 'translateX(0)',
+        },
+        {
+          opacity: '0',
+          transform: 'translateX(200px)',
+        }
+      ], {
+        duration: 500,
+        easing: 'ease',
+      });
+
+      animation.onfinish = Appfairy.Socket.prototype.hide.bind(this, proxy);
+    };
+  }
+}
+
+Appfairy.View.define('todo-list', TodoListView);
+
+export default TodoListView;
